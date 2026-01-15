d690bafd5fdce76ad704f8861cf7d5bbf8238933
Maksym Penkov <ipz231_pmo@student.ztu.edu.ua>
2026-01-15 17:28:02 +0200

    added initial routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import {
-    ModalLayout,
-    Login,
-    Header
-} from "./components";
 import {useState} from "react";
 import "./App.css";
+import {HomePage, SubcategoryPage} from "./pages";
+import {AppContext} from "./AppContext.jsx";
+import {BrowserRouter, Route, Routes} from "react-router";
 
 function App() {
-
-    const [isLoginOpen, setIsLoginOpen] = useState(true);
-
+    const [context, setContext] = useState({userToken: null, theme: "light"});
     return (
-        <>
-            <Header />
-            <ModalLayout isOpen={isLoginOpen}>
-                <Login onClose={() => setIsLoginOpen(false)} />
-            </ModalLayout>
-        </>
+        <AppContext.Provider value={{context, setContext}} >
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/subcategory/:id" element={<SubcategoryPage />} />
+                </Routes>
+            </BrowserRouter>
+        </AppContext.Provider>
     )
 }
 
